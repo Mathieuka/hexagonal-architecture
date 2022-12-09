d83088edeffebd19f85bf89ad52c9659b77d5e04
MK <mathieu.kahlaoui.pro@gmail.com>
2022-12-09 11:55:07 +0800

    Create test file for Post service

---
+++ src/__tests__/post.service.test.ts
@@ -0,0 +1,8 @@
+import { postService } from "@domain/services/Post.services";
+import { postRepository } from "@infrastructure/repositories/postRepository";
+
+describe("Post service", () => {
+  test("Dummy", () => {
+    expect(1).toEqual(1);
+  });
+});
--- src/domain/services/__tests__/post.service.test.ts
+++
@@ -1,5 +0,0 @@
-describe("Post service", () => {
-  test("Dummy", () => {
-    expect(1).toEqual(1);
-  });
-});
